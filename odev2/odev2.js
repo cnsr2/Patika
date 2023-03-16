9f03c6a4e5d612c34c1eb6e6eb328ba12dcd217f
Emre Can <cansarrr2@gmail.com>
2023-03-17 02:01:19 +0300

    düzenlemeler yapıldı

--- odev2/odev2.js
+++ odev2/odev2.js
@@ -1,12 +1,12 @@
-const postlar = [{ name: "Post1" },
+let postlar = [{ name: "Post1" },
 { name: "Post2" },
 { name: "Post3" }]
 
-function postEkle(yeniPost) {
-    postlar.push(yeniPost)
-    const yeniList = postlar.map(function (x) {
+function postEkle(...yeniPost) {
+    postlar = [...postlar, ...yeniPost]
+    const yeniList = postlar.map((x) => {
         return x.name;
     });
     console.log(yeniList);
 }
-postEkle({ name: "Post4" });
+postEkle({ name: "Post4" },{ name: "Post5" });
