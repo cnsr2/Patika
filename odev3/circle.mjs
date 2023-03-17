1d8db25e62bacf8d41cc2504969a31ba76c1abf0
Emre Can <cansarrr2@gmail.com>
2023-03-17 22:32:11 +0300

    odev3 yapıldı

---
+++ odev3/circle.mjs
@@ -0,0 +1,7 @@
+export function circleArea(yariCap){
+    return yariCap * yariCap * 3;
+};
+
+export default function circleCircumference(yariCap){
+    return 2 * 3 *yariCap;
+};
---
+++ odev3/index.mjs
@@ -0,0 +1,4 @@
+import x,{circleArea} from "./circle.mjs";
+
+console.log(x(5));
+console.log(circleArea(5));
