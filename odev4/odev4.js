0c220e8ae84d67f1c90fffefe4883071ace474a8
Emre Can <cansarrr2@gmail.com>
2023-03-22 21:30:44 +0300

    Odev4 yapıldı

---
+++ odev4/odev4.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+    // -- burada yeni bir employees.json dosyası oluşturup içini dolduruyoruz 
+    //  fs.appendFile("./employees.json", '{"name": "Employee 1 Name", "salary": 2000}', (err) => {
+    //      if (err) throw err;
+    //  })
+
+    // -- burada oluşturduğumuz dosyayı okuyoruz
+
+    // fs.readFile("./employees.json",'utf8',(err,data) => {
+    //     if (err) throw err;
+    //     console.log(data);
+    // });
+
+    // -- burada dosyamızın içeriğini güncelliyoruz 
+
+    // fs.writeFile("./employees.json",'{"sale":20}"','utf-8',(err) => {
+    //     if (err) throw err;
+    //     console.log("Dosya'ya Güncellendi");
+    // });
+    fs.unlink("./employees.json",(err) => {
+        if (err) throw err;
+    });
